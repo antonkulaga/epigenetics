8e53a75fdc47255e3440ba1c60aa967a3bfe9edf
Anton Kulaga <antonkulaga@gmail.com>
2021-06-02 03:23:06 +0300

    chip-seq-r container

---
+++ containers/chip-seq-r/install.R
@@ -0,0 +1,11 @@
+general <- c("tidyverse")
+BiocManager::install(general)
+
+chip_seq <- c("csaw", "ChIPseeker", "clusterProfiler", "DiffBind", "edgeR", "DESeq2", "ChIPQC")
+BiocManager::install(chip_seq)
+
+medips <- c("MEDIPS", "RUVSeq", "DSS")
+BiocManager::install(medips)
+
+annotations <- c("BSgenome", "BSgenome.Hsapiens.NCBI.GRCh38", "AnnotationDbi", "AHEnsDbs")
+BiocManager::install(annotations)
